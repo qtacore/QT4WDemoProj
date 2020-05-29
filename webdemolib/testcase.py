094b289eed6b7c21ab28b72ffc588b3f2f9326a3
drunkdream <drunkdream@qq.com>
2020-05-29 15:47:27 +0800

    支持配置代理和hosts

--- webdemolib/testcase.py
+++ webdemolib/testcase.py
@@ -3,6 +3,9 @@
 '''
 #2019/01/18 QTAF自动生成
 
+import random
+
+from qt4w.browser import Browser
 from testbase.conf import settings
 
 if settings.QT4W_PLATFORM == 'Android':
@@ -27,4 +30,31 @@ else:
 class WebDemoTestCase(WebTestBase):
     '''Web demo测试用例基类
     '''
-    pass
+
+    def pre_test(self):
+        super(WebDemoTestCase, self).pre_test()
+        try:
+            from qt4w.util import HostsManager, ProxyServer
+        except ImportError:
+            return
+        if hasattr(settings, 'QT4W_HTTP_PROXY') and settings.QT4W_HTTP_PROXY:
+            # 设置代理服务器
+            Browser.proxy_server = settings.QT4W_HTTP_PROXY
+        elif hasattr(settings, 'QT4W_HOSTS') and settings.QT4W_HOSTS:
+            hosts_manager = HostsManager()
+            hosts_manager.add_hosts(settings.QT4W_HOSTS)
+            proxy_port = random.randint(10000, 60000)
+            self.proxy_server = ProxyServer(proxy_port)
+            self.proxy_server.start_in_thread()
+            Browser.proxy_server = 'http://127.0.0.1:%d' % proxy_port
+
+    def post_test(self):
+        super(WebDemoTestCase, self).post_test()
+        try:
+            from qt4w.util import HostsManager
+        except ImportError:
+            return
+        hosts_manager = HostsManager()
+        hosts_manager.clear_hosts()
+        if hasattr(self, 'proxy_server') and self.proxy_server:
+            self.proxy_server.stop()
