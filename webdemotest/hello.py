ce8d02bd774935f14d60e481d57f9be11d2576cf
drunkdream <drunkdream@qq.com>
2022-07-22 11:59:42 +0800

    Add UploadFileTest

--- webdemotest/hello.py
+++ webdemotest/hello.py
@@ -3,11 +3,15 @@
 """
 # 2019/01/18 QTAF自动生成
 
+import os
+import random
+import tempfile
 import time
 
 from qt4w.browser import Browser
 
 from webdemolib.demopage import DemoPage, ProfilePage
+from webdemolib.uploadpage import UploadFilePage
 from webdemolib.testcase import WebDemoTestCase
 
 
@@ -42,6 +46,29 @@ class WebDemoTest(WebDemoTestCase):
         self.assert_equal("检查性别", page.control("性别").inner_text, "女")
         self.assert_equal("检查年龄", page.control("年龄").inner_text, "20")
         self.assert_equal("检查公司", page.control("公司").inner_text, "tencent")
+
+
+class UploadFileTest(WebDemoTestCase):
+    """上传文件测试"""
+
+    owner = "drunkdream"
+    timeout = 5
+    priority = WebDemoTestCase.EnumPriority.High
+    status = WebDemoTestCase.EnumStatus.Ready
+
+
+    def run_test(self):
+        self.start_step("1. 在浏览器中打开上传文件测试页面")
+        browser = Browser()
+        page = browser.open_url("https://qtacore.github.io//qt4w/upload.html", UploadFilePage)
+
+        file_path = tempfile.mkstemp(".txt")[1]
+        file_size = random.randint(1024, 10240)
+        with open(file_path, "w") as fp:
+            fp.write("a" * file_size)
+        page.upload_file(file_path)
+        self.assert_equal("检查要上传的文件名", page.control("文件名").inner_text, os.path.split(file_path)[-1])
+        self.assert_equal("检查要上传的文件大小", int(page.control("文件大小").inner_text), file_size)
 
 
 if __name__ == "__main__":
